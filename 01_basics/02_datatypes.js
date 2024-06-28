4b4b6949c3eab5b00e10afc8110ce742ee7b55f3
geekaashu <ashu.vis.6123@gmail.com>
2024-06-28 08:36:06 +0000

    data types done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+let name = "aashu"
+let age = 23
+let isLoggedin = false
+
+// number = 2
+// bigint
+// string = ""
+// boolean = true/false
+// null= standalone value 
+// undefined =
+// symbol = 
+
+console.log(typeof null); // underined
+console.log(typeof null); // object
