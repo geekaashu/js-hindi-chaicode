829598ddbaad80e04e09800419b6a1a380e1a347
geekaashu <ashu.vis.6123@gmail.com>
2024-06-28 08:38:23 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 5644564324
+let accountEmail = "aashu@gmail.com"
+var accountPassword = "1234"
+accountCity = "Indore"
+
+
+//accountId = 23 not allowed
+accountEmail = "aashutosh@gmail.com"
+accountPassword = "4321"
+accountCity = "Delhi"
+
+
+/*
+prefer not to use var 
+/*
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity]);
