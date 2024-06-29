f11332944df0f0782bf5c1a10dfe3193450f3ba4
geekaashu <ashu.vis.6123@gmail.com>
2024-06-29 08:26:03 +0000

    Updated

---
+++ 01_basics/01_conversionandoperation.js
@@ -0,0 +1,74 @@
+let score = "aashu"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+//xxzgvxwe can convert variable into number but if it is not in scope and if we try to print the converted number  it will give an error as NAN
+
+//33 = number but 33adada = NAN
+
+//True = 1
+
+//False = 0
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanIsLoggedIn);
+
+//we can conver number value into boolean with this but the number should be 0 or 1
+
+// 0 = false 1 = true empty string = false string = true
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+
+console.log(StringNumber);
+
+console.log(typeof(StringNumber))
+
+
+//******************operations************ */
+
+let value = 33
+
+let nagValue = -value
+
+console.log(nagValue)
+
+let str1 = "aashutosh"
+
+let str2 = " vishwakarma"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2)
+
+
+console.log(2 + 2 + "1")
+
+console.log("2" + 1 + 2)
+
+
+//this is how addition operation works
+
+console.log(+true)
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter)
+
+
+
