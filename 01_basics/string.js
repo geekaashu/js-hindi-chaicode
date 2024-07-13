d7f55189d8531696f493062cabb6371a0777b744
geekaashu <ashu.vis.6123@gmail.com>
2024-07-13 10:05:55 +0000

    new topic added

---
+++ 01_basics/string.js
@@ -0,0 +1,75 @@
+const name = " ashutosh "
+
+const repocount = 50
+
+console.log(name + repocount)
+
+console.log(name + repocount +" value")
+
+
+//this is a outdated way to use string
+
+
+console.log(`Hello my name is ${name} and repocount is ${repocount}`)
+
+//This is the modedrn way to use the string in java
+
+
+const gamename = new String('    aashu')
+
+console.log(typeof gamename)
+
+console.log(gamename[3])
+
+//we can acess the key value pairs with this method
+
+
+console.log(gamename.length)
+
+
+console.log(gamename.toUpperCase())
+
+
+console.log(gamename.charAt(2))
+
+
+console.log(gamename.indexOf('s'))
+
+const newstring = gamename.substring(0, 3)
+
+
+console.log(newstring)
+
+
+console.log(gamename.trim())
+
+
+const myurl = "www.ashutosh.com/%20mybooks/element.com"
+
+
+
+console.log(myurl.replace('%20', '-'))
+
+
+console.log(gamename.includes('shu'))
+
+
+
+console.log(gamename.includes('shggu'))
+
+
+const splitvalue = "ashutosh-vishwakarma-indore"
+
+splited = splitvalue.split('-')
+
+console.log(splited)
+
+
+
+
+
+
+
+
+
+
